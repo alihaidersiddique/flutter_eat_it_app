4da248c574bba2d40e95de43a3f34ac4256ee3da
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2021-09-16 22:31:15 +0500

    style restaurant list completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:auto_animated/auto_animated.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_eat_it_app/model/restaurant_model.dart';
 import 'package:flutter_eat_it_app/strings/main_strings.dart';
 import 'package:flutter_eat_it_app/view_model/main_view_model_imp.dart';
+import 'package:flutter_eat_it_app/widget/common/common_widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'widget/main/main_widgets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   FirebaseApp app = await Firebase.initializeApp();
 
   runApp(MyApp(app: app));
@@ -59,11 +63,36 @@ class MyHomePage extends StatelessWidget {
             );
           else {
             var lst = snapshot.data as List<RestaurantModel>;
-            return ListView.builder(
-              itemCount: lst.length,
-              itemBuilder: (context, index) {
-                return Text(lst[index].name);
-              },
+            return Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: LiveList(
+                showItemDuration: Duration(milliseconds: 350),
+                showItemInterval: Duration(milliseconds: 150),
+                reAnimateOnVisibility: true,
+                scrollDirection: Axis.vertical,
+                itemCount: lst.length,
+                itemBuilder: animationItemBuilder(
+                  (index) => InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height / 2.5 * 1,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RestaurantImageWidget(
+                            imageUrl: lst[index].imageUrl,
+                          ),
+                          RestaurantInfoWidget(
+                            name: lst[index].name,
+                            address: lst[index].address,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             );
           }
         },
