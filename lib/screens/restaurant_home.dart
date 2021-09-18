d192e1683d80de6243fb10b3c12ee0af57541f3c
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2021-09-18 12:44:35 +0500

    Style Most Popular Categories completed

--- lib/screens/restaurant_home.dart
+++ lib/screens/restaurant_home.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_eat_it_app/model/popular_item_model.dart';
 import 'package:flutter_eat_it_app/state/main_state.dart';
-import 'package:flutter_eat_it_app/strings/restaurant_home_strings.dart';
 import 'package:flutter_eat_it_app/view_model/restaurant_home_vm/restaurant_home_vm.dart';
 import 'package:flutter_eat_it_app/view_model/restaurant_home_vm/restaurant_home_vm_imp.dart';
+import 'package:flutter_eat_it_app/widget/restaurant%20home/most_popular_widget.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -17,45 +16,23 @@ class RestaurantHome extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('${mainStateController.selectedRestaurant.value.name}'),
+          title: Text('${mainStateController.selectedRestaurant.value.name}',
+              style: GoogleFonts.jetBrainsMono(color: Colors.black)),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 10,
+          iconTheme: IconThemeData(color: Colors.black),
         ),
         body: Container(
           padding: const EdgeInsets.all(8),
           child: Column(
             children: [
               Expanded(
-                  flex: 1,
-                  child: Container(
-                    child: FutureBuilder(
-                      future: viewModel.displayMostPopularByRestaurantId(
-                          mainStateController
-                              .selectedRestaurant.value.restaurantId),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting)
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        else {
-                          var lstPopular =
-                              snapshot.data as List<PopularItemModel>;
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                mostPopularText,
-                                style: GoogleFonts.jetBrainsMono(
-                                  fontWeight: FontWeight.w900,
-                                  fontSize: 24,
-                                  color: Colors.black45,
-                                ),
-                              ),
-                              Text('Popular length: ${lstPopular.length}')
-                            ],
-                          );
-                        }
-                      },
-                    ),
-                  )),
+                flex: 1,
+                child: MostPopularWidget(
+                    viewModel: viewModel,
+                    mainStateController: mainStateController),
+              ),
               Expanded(flex: 2, child: Container(color: Colors.blue))
             ],
           ),
