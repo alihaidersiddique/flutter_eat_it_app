52f057c2b4c36ced66183951f4df762cde07c8cb
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2021-09-21 15:25:57 +0500

    style food list completed

--- lib/screens/food_list.dart
+++ lib/screens/food_list.dart
@@ -1,5 +1,10 @@
+import 'package:auto_animated/auto_animated.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_eat_it_app/const/const.dart';
 import 'package:flutter_eat_it_app/state/category_state.dart';
+import 'package:flutter_eat_it_app/strings/food_list_strings.dart';
+import 'package:flutter_eat_it_app/widget/common/common_widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,9 +25,103 @@ class FoodListScreen extends StatelessWidget {
           iconTheme: IconThemeData(color: Colors.black),
           elevation: 10,
         ),
-        body: Center(
-          child: Text(
-              'We have ${categoryStateController.selectedCategory.value.foods.length} foods in this category.'),
+        body: Column(
+          children: [
+            Expanded(
+                child: LiveList(
+              showItemDuration: Duration(milliseconds: 300),
+              showItemInterval: Duration(milliseconds: 300),
+              reAnimateOnVisibility: true,
+              scrollDirection: Axis.vertical,
+              itemCount:
+                  categoryStateController.selectedCategory.value.foods.length,
+              itemBuilder: animationItemBuilder((index) => InkWell(
+                  onTap: () {},
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height / 6 * 2,
+                    child: Card(
+                      semanticContainer: true,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl: categoryStateController
+                                .selectedCategory.value.foods[index].image,
+                            fit: BoxFit.cover,
+                            errorWidget: (context, url, err) => Center(
+                              child: Icon(Icons.image),
+                            ),
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              color: Color(COLOR_OVERLAY),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 10, bottom: 5),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Text(
+                                              '${categoryStateController.selectedCategory.value.foods[index].name}',
+                                              textAlign: TextAlign.center,
+                                              style: GoogleFonts.jetBrainsMono(
+                                                  color: Colors.white,
+                                                  fontSize: 18),
+                                            ),
+                                            Text(
+                                              '$priceText\$${categoryStateController.selectedCategory.value.foods[index].price}',
+                                              textAlign: TextAlign.center,
+                                              style: GoogleFonts.jetBrainsMono(
+                                                  color: Colors.white,
+                                                  fontSize: 18),
+                                            ),
+                                            Row(
+                                              children: [
+                                                IconButton(
+                                                  onPressed: () {},
+                                                  icon: Icon(
+                                                      Icons.favorite_border,
+                                                      color: Colors.white),
+                                                ),
+                                                SizedBox(width: 50),
+                                                IconButton(
+                                                  onPressed: () {},
+                                                  icon: Icon(
+                                                      Icons.add_shopping_cart,
+                                                      color: Colors.white),
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ))),
+            ))
+          ],
         ));
   }
 }
--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -104,7 +104,6 @@ class MyHomePage extends StatelessWidget {
           }
         },
       ),
-    
     );
   }
 }
